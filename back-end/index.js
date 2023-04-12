2038dcf916edb7c7289d8d086f69bdacac1859bf
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-04-12 22:01:17 +0700

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("This is my API")
+    res.send("This is my new API")
 })
 
 const { authRouter } = require("./routers")

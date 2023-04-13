1194e838cc45fdd22ad26023788157e18bfc984e
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-04-13 21:31:13 +0700

    add feature update product

--- back-end/routers/productRouters.js
+++ back-end/routers/productRouters.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const { productControllers } = require("../controllers")
 
 router.post("/create", productControllers.createProduct);
+router.patch("/update/:productId", productControllers.updateProduct);
 router.get("/", productControllers.getProducts);
 router.post("/category", productControllers.createCategory);
 

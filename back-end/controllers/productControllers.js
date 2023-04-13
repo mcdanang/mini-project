1c83e58253a0b2778174b719b3ead276efb36505
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-04-13 20:43:49 +0700

    add feature create product

--- back-end/controllers/productControllers.js
+++ back-end/controllers/productControllers.js
@@ -1,8 +1,26 @@
 const db = require("../models");
-// const product = db.Product;
+const product = db.Product;
 const category = db.Category;
 
 module.exports = {
+    createProduct : async (req, res) => {
+        try {
+            const { name, description, price, category_id, image_url, store_id } = req.body;
+            if (!name || !description || !price || !category_id || !image_url || !store_id) {
+                throw "Please complete required product data"
+            }
+
+            const newProduct = await product.create({
+                name, description, price, category_id, image_url, store_id
+            })
+            res.status(200).send({
+                message: "Product successfully created",
+            })
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(err)
+        }
+    },
     getProducts : async (req, res) => {
         try {
             res.status(200).send({

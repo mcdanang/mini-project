98d608f5e0c1b386ead13baf460ba00a6a835302
ChristianTanaka251299 <christian.tanaka2512@gmail.com>
2023-05-02 21:02:15 +0700

    fix cart from single item to multiple items

--- back-end/controllers/transactionControllers.js
+++ back-end/controllers/transactionControllers.js
@@ -1,64 +1,69 @@
-const {Sequelize} = require("sequelize")
-const db = require("../models")
-const Product = db.Product
-const transDetail = db.Transaction_detail
-const transHead = db.Transaction_header
+const { Sequelize } = require("sequelize");
+const db = require("../models");
+const Product = db.Product;
+const transDetail = db.Transaction_detail;
+const transHead = db.Transaction_header;
 
 module.exports = {
-    createTransaction: async(req,res) => {
-        try{
-            const { product_name, qty} = req.body
+  createTransaction: async (req, res) => {
+    try {
+      const { cart } = req.body;
+      const totalPrice = cart.reduce((total, product) => {
+        return total + product.product_price * product.qty;
+      }, 0);
 
-            const product = await Product.findOne({where : {name: product_name}})
-            
-            const headerTransaction = await transHead.create({
-                total_price: product.price * qty,
-                date: new Date().toString(), 
-                user_id: req.userId,
-            })
-            const newTransaction = await transDetail.create({
-                qty,
-                product_name,
-                product_price: product.price,
-                ProductId:product.id,
-                TransactionHeaderId: headerTransaction.id
-            })
-            res.status(200).send({
-                message: "Transaction successfully created",
-                data: {
-                    Transaction_Header: headerTransaction,
-                    Transaction_detail: newTransaction
-                }
-            })
-        } catch (err){
-            console.log(err)
-            res.status(500).send(err)
-      
-       }
-    },
+      const transactionHeader = await transHead.create({
+        total_price: totalPrice,
+        date: new Date(),
+        user_id: req.userId,
+      });
 
-    deleteTransaction: async(req,res) => {
-        try{
-            const transactionDone = await transHead.findOne({
-                where: {id: req.params.id},
-            })
-            
-            // Delete the associated Transaction_detail record
-            await transDetail.destroy({ where: { TransactionHeaderId: transactionDone.id } });
-            
-            // Delete the Transaction_header record
-            await transHead.destroy({ where: { id: transactionDone.id } });
-            
-            res.status(200).send({
-                message: "Transaction successfully deleted",
-                data: {
-                    Transaction_Header: transactionDone
-                }
-            })
-        }catch (err){
-            console.log(err)
-            res.status(500).send(err)
-        }
+      const newTransactions = await transDetail.bulkCreate(
+        cart.map((product) => {
+          return {
+            qty: product.qty,
+            product_name: product.product_name,
+            product_price: product.product_price,
+            ProductId: product.ProductId,
+            TransactionHeaderId: transactionHeader.id,
+          };
+        })
+      );
+
+      res.status(200).send({
+        message: "Transaction successfully created",
+        data: {
+          Transaction_Header: transactionHeader,
+          Transaction_detail: newTransactions,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
     }
+  },
 
-}
+  deleteTransaction: async (req, res) => {
+    try {
+      const transactionDone = await transHead.findOne({
+        where: { id: req.params.id },
+      });
+
+      await transDetail.destroy({
+        where: { TransactionHeaderId: transactionDone.id },
+      });
+
+      await transHead.destroy({ where: { id: transactionDone.id } });
+
+      res.status(200).send({
+        message: "Transaction successfully deleted",
+        data: {
+          Transaction_Header: transactionDone,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+  },
+};
